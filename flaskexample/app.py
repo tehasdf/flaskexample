8c6599f9a1069a76ca1d1cfd11f4cac19391b0f5
asdf <tehasdf@gmail.com>
2016-01-13 00:29:10 +0100

    add database

--- flaskexample/app.py
+++ flaskexample/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 
 from flaskexample.views import main
+from flaskexample.models import db
 
 
 def create_app(config=None):
     app = Flask(__name__)
+    db.init_app(app)
     app.register_blueprint(main)
     return app
---
+++ flaskexample/models.py
@@ -0,0 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+
+class Category(db.Model):
+    category_id = db.Column(db.Integer(), primary_key=True)
+
+    name = db.Column(db.String(), max_length=1000)
+
+
+class Product(db.Model):
+    product_id = db.Column(db.Integer(), primary_key=True)
+
+    name = db.Column(db.String(), max_length=1000)
+    description = db.Column(db.String(), max_length=10000)
+    price = db.Column(db.Numeric())
+
+
