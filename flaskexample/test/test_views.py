169814918f0d41441f626373326c07358ed45d55
asdf <tehasdf@gmail.com>
2016-01-23 16:05:41 +0100

    document views & view tests

--- flaskexample/test/test_views.py
+++ flaskexample/test/test_views.py
@@ -1,3 +1,10 @@
+# coding: utf-8
+
+# show several testing strategies for flask views
+
+# this uses the fixtures defined in conftest.py; you can also use the
+# pytest-flask library, or flask-testing
+
 import pytest
 
 from werkzeug.datastructures import MultiDict
@@ -5,18 +12,35 @@ from flaskexample.views import calculate_result, BarForm
 from flaskexample.models import Category
 
 
+# a simple way to do TDD with flask, is to factor out the tricky/hard parts of
+# code into separate functions that have nothing to do with flask, and test
+# those first.
+# not having to interact with flask, but rather testing simple python code,
+# makes this considerably easier
+
+# the "foo" route in views.py, extracts a number from the request data,
+# passes it to a separate function which does the actual calculation,
+# and then displays the result.
+# this function only tests that separate function, which is "the hard part",
+# in isolation
 def test_calculate_view():
     assert calculate_result(5) == 120
 
 
-def test_using_test_client(client):
-    result = client.post('/foo', data={'n': 5})
-    assert result.data == 'result is 120'
-
-
+# for when a function like this needs app context; usually it's better to
+# not do this; eg. if your function needs access to app config, instead
+# of using current_app, simply retrieve the config value outside of the
+# function, and pass the value in
 def test_creating_appctx(db, app):
     with app.app_context():
         categories = Category.query.all()
+
+
+# a very direct way to test is to use flask's test client,
+# using the fixtures, or eg. `with app.test_client() as c: result = c.get()`
+def test_using_test_client(client):
+    result = client.post('/foo', data={'n': 5})
+    assert result.data == 'result is 120'
 
 
 def test_bar_view(client):
@@ -29,8 +53,14 @@ def test_bar_view_too_large(client):
     assert result.status_code == 400
 
 
+# pytest allows creating a fixture that will make the whole test be in app context
 @pytest.mark.usefixtures('appctx')
 def test_bar_just_form(app):
+    # this test only checks the form, which is the part of the view that should always
+    # be tested
+    # even if you say you don't have time for good coverage, always do test your
+    # forms at least
+
     data = MultiDict({'n': 9})
     form = BarForm(formdata=data, csrf_enabled=False)
     assert form.validate()
--- flaskexample/views.py
+++ flaskexample/views.py
@@ -1,3 +1,8 @@
+# coding: utf-8
+
+# this is a plain and simple views module, structured using a blueprint
+# for more info on blueprints, see http://flask.pocoo.org/docs/dev/blueprints/
+
 import json
 
 from flask import Blueprint, render_template, request, render_template_string, abort
@@ -17,10 +22,15 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
+    # note that this uses both the Product, and the GenericProduct, defined
+    # using separate db.Models; all that was needed was to simply init_app the
+    # generic db, which allowed declaring a FK to the library model
     products = Product.query.join(GenericProduct).all()
     return render_template("index.html", products=products)
 
 
+
+# some simple POST handlers; also see their tests in flaskexample/test/test_views.py
 def calculate_result(n):
     fact = 1
     for i in range(1, n + 1):
@@ -53,13 +63,14 @@ def bar():
         abort(400)
 
 
+# route using the redis extension from myext.py
 @main.route('/publish')
 def publish():
     publisher.publish('channel', {'some': 'data'})
     return ('ok, published', 200, {'Content-Type': 'text/plain'})
 
 
+# route using the simple extension from myext.py
 @main.route('/simple')
 def show_simple():
     return render_template_string('simple is {{ simple }}', simple=simple)
-
